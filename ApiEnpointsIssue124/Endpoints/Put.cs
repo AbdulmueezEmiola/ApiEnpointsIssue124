669fe0fa4f84f22dfda68aaaa9659bb86b82dc77
Santor, Maurice <msantor@users.noreply.github.com>
2021-08-23 08:51:41 -1000

    Add project files.

---
+++ ApiEnpointsIssue124/Endpoints/Put.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEnpointsIssue124.Endpoints
+{
+	public class Put : Ardalis.ApiEndpoints.BaseAsyncEndpoint.WithRequest<RequestType>.WithoutResponse
+	{
+		[HttpPut("/put/{Id}")]
+		public override async Task<ActionResult> HandleAsync([FromRoute] RequestType request, CancellationToken cancellationToken = default)
+		{
+			Debug.Assert(request.Id != Guid.Empty);
+
+			await Task.Delay(0, cancellationToken);
+
+			return Ok();
+		}
+	}
+
+	public class RequestType
+{
+		[FromRoute(Name = "Id")] public Guid Id { get; set; }
+		[FromBody] public byte[] Version { get; set; }
+		[FromBody] public string FromBody1 { get; set; }
+		[FromBody] public string FromBody2 { get; set; }
+	}
+}
